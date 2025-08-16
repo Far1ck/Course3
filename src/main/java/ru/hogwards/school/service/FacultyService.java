580d2ffc6b4a82fd0321f27cd4b8abe74474f38f
Far1ck <salyakhov.farit@gmail.com>
2025-08-17 00:13:36 +0500

    Homework

--- src/main/java/ru/hogwards/school/service/FacultyService.java
+++ src/main/java/ru/hogwards/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwards.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwards.school.model.Faculty;
 import ru.hogwards.school.model.Student;
@@ -10,6 +12,8 @@ import java.util.NoSuchElementException;
 
 @Service
 public class FacultyService {
+
+    Logger logger = LoggerFactory.getLogger(FacultyService.class);
     private FacultyRepository facultyRepository;
 
     public FacultyService(FacultyRepository facultyRepository) {
@@ -17,30 +21,39 @@ public class FacultyService {
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method \"createFaculty\"");
         return facultyRepository.save(faculty);
     }
 
     public Faculty getFacultyById(Long id) {
+        logger.info("Was invoked method \"getFacultyById\"");
+        logger.warn("This method may return null");
         return facultyRepository.findById(id).orElse(null);
     }
 
     public Faculty updateFaculty(Faculty faculty) {
+        logger.info("Was invoked method \"updateFaculty\"");
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(Long id) {
+        logger.info("Was invoked method \"deleteFaculty\"");
         facultyRepository.deleteById(id);
     }
 
     public List<Faculty> filterByColor(String color) {
+        logger.info("Was invoked method \"filterByColor\"");
         return facultyRepository.findAllByColor(color);
     }
 
     public List<Faculty> findFacultyByNameOrByColor(String query) {
+        logger.info("Was invoked method \"findFacultyByNameOrByColor\"");
         return facultyRepository.findAllByNameIgnoreCaseOrColorIgnoreCase(query, query);
     }
 
     public List<Student> getAllStudentsOfTheFacultyById(Long id) {
+        logger.info("Was invoked method \"getAllStudentsOfTheFacultyById\"");
+        logger.warn("There may be no faculty with such ID");
         return facultyRepository.findById(id).orElseThrow(NoSuchElementException::new).getStudents();
     }
 }
